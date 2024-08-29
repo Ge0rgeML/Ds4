f0e1766f9b35db801248ceb5b4b509cceb30b5fd
jorge mendoza <georgejorgemendoza@hotmail.com>
2024-08-28 19:22:00 -0500

    Creacion Lab 2 y ejemplo 1

---
+++ Laboratorio2/Ejemplo1.cs
@@ -0,0 +1,25 @@
+﻿internal class Ejemplo1
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "Nombre";
+        client.LastName = "Apellido";
+        client.Age = 15;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
