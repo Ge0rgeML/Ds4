76c32146b35b3792b5f646f9c038083c61d31486
jorge mendoza <georgejorgemendoza@hotmail.com>
2024-09-02 20:04:16 -0500

    Laboratorio 3 problemas 31,32,33

---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    public class CalculosMatematicos
+    {
+        public const double PI = 3.14159;
+        public int Resultado(int numero1, int numero2)
+        {
+            return (numero1 + numero2) * (numero1 - numero2);
+        }
+        public double AreaC(double r)
+        {
+            return PI * Math.Pow(r, 2);
+        }
+        public double PerimetroR(double l,double a)
+        {
+            return 2 * (l+a);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        int num1, num2;
+
+        Console.WriteLine("ingrese el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ingrese el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos calmat = new CalculosMatematicos();
+        int resultado = calmat.Resultado(num1, num2);
+        Console.WriteLine($"Resultado de ({num1} + {num2}) * ({num1} - {num2}) es: {resultado} ");      
+    }
+}
---
+++ Laboratorio3/Laboratorio32/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public class CalculosMatematicos
+    {
+        public const double PI = 3.14159;
+        public int Resultado(int numero1, int numero2)
+        {
+            return (numero1 + numero2) * (numero1 - numero2);
+        }
+        public double AreaC(double r)
+        {
+            return PI * Math.Pow(r, 2);
+        }
+        public double PerimetroR(double l, double a)
+        {
+            return 2 * (l + a);
+        }
+    }
+    private static void Main(string[] args)  
+    {
+        double radio;
+
+    Console.WriteLine("ingrese el radio de su circulo para saber el area: ");
+        radio = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos calmat = new CalculosMatematicos();
+    double resultado = calmat.AreaC(radio);
+    Console.WriteLine($"El area del circulo con radio de {radio} es: {resultado} ");
+    }
+}
