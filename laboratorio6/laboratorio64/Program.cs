20abb39b5ef99bf60c6f9c39a2456fc7d05bb00d
jorge mendoza <georgejorgemendoza@hotmail.com>
2024-09-16 18:55:40 -0500

    laboratorio 6 con los problemas

---
+++ laboratorio6/laboratorio64/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+internal class Program
+{
+    static void checkAge(int age)
+    {
+        if(age < 18)
+        {
+            throw new ArithmeticException("Acceso negado - no cmple con el criterio de edad");
+
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        checkAge(15);
+    }
+}
