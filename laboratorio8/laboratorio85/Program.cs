ec53df09ef80b110c7d147378ac75873eee34170
jorge mendoza <georgejorgemendoza@hotmail.com>
2024-09-19 00:12:49 -0500

    laboratorio 8 termiando

--- laboratorio8/laboratorio85/Program.cs
+++ laboratorio8/laboratorio85/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using laboratorio85;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Coordenadas misCoords = new Coordenadas(10, 15);
+        misCoords.VerCoordenadas();
     }
 }
